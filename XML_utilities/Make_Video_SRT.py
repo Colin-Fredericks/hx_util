dea9a0e1d5b55545033782e95fd9527a8325cffa
Colin-Fredericks <colin.fredericks@gmail.com>
2017-07-18 16:47:00 -0400

    Switching to recursive method

--- XML_utilities/Make_Video_SRT.py
+++ XML_utilities/Make_Video_SRT.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import collections
 import csv
 import sys
 import os
@@ -14,6 +15,31 @@ location of each video, and the srt filename for that video.
 (TSV = tab-separated value. Open with Excel.)
 """
 
+# Recursion function for outline-declared xml files
+# (doesn't actually recurse yet)
+def drillDown(folder,filename):
+    tempOD = collections.OrderedDict()
+
+    tree = ET.parse(folder + '/' + filename + '.xml')
+    root = tree.getroot()
+
+    for index, child in enumerate(root):
+        tempOD[folder][index] = collections.OrderedDict()
+        if 'display_name' in child.attrib:
+            tempOD[folder][index]['name'] = child.attrib['display_name']
+        else:
+            tempOD[folder][index]['name'] = folder + str(index)
+
+        if 'url_name' in child.attrib:
+            tempOD[folder][index]['url'] = child.attrib['url_name']
+
+    return tempOD
+
+
+# Recursion function for inline-declared XML.
+# This is a placeholder.
+
+
 # Get the filename
 try:
     coursefile = sys.argv[1]
@@ -21,18 +47,30 @@ except IndexError:
     # If run without argument, show instructions.
     sys.exit(instructions)
 
+# We need lists of container nodes and leaf nodes so we can tell
+# whether we have to do more recursion.
+leaf_nodes = ['html','problem','video','poll']
+branch_nodes = ['course','chapter','sequential','vertical','split_test']
+
+# This is the ordered dict where we're storing the course structure.
+# Later we'll dump it out to the tab-separated file.
+course_dict = collections.OrderedDict()
+
 # Open course's root xml file
-with open (coursefile, 'rb') as courseroot:
-    # Get the current course run filename
-    # Open the course run file
+# Get the current course run filename
+root_tree = ET.parse(coursefile)
+root_root = root_tree.getroot()
 
-# Chapter file
+course_dict['name'] = root_root.attrib['course']
+course_dict['url'] = root_root.attrib['url_name']
+course_dict['chapters'] = drillDown('course',course_dict['url'])
 
-# Sequential file
+print course_dict
 
-# Vertical file
-
-# Video file or inline video declaration in vertical
+for chapter in course_dict['chapters']:
+    # Open the Chapter files
+    chapter_tree = ET.parse('chapter/' + chapter['url'] + '.xml')
+    chapter_root = chapter_tree.getroot()
 
 # Make the file's header row
 
