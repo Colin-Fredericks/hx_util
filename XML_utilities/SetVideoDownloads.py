355ccdf4e0f0a8c6a551a0a41e3136713058edff
Colin-Fredericks <colin.fredericks@gmail.com>
2017-08-23 14:50:05 -0400

    Script to set video download option

---
+++ XML_utilities/SetVideoDownloads.py
@@ -0,0 +1,71 @@
+# import XML libraries
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+import HTMLParser
+import sys
+import os
+
+instructions = """
+To use:
+python SetVideoDownloads.py option path/to/video/folder
+
+Set your option as...
+  "true" to allow downloads of videos and transcripts for every video.
+  "false" to disallow downloads of videos and transcripts for every video.
+  "transcript" to allow only transcripts to be downloaded.
+  "video" to allow only videos to be downloaded.
+  "reset" to clear this attribute and let the edX defaults take over.
+
+"""
+
+# Here are all the problem types we work on:
+allProbtypes = ['multiplechoiceresponse',
+    'choiceresponse',
+    'customresponse',
+    'formularesponse',
+    'numericalresponse',
+    'stringresponse',
+]
+
+
+
+# Get directory from command line argument
+try:
+    allowDownloads = sys.argv[1]
+    directory = sys.argv[2]
+except IndexError:
+    # Probably the wrong number of arguments
+    sys.exit(instructions)
+
+# Walk through the problems folder
+for dirpath, dirnames, filenames in os.walk(directory):
+    for eachfile in filenames:
+
+        # Get the XML for each file
+        tree = ET.parse(os.path.join(dirpath, eachfile))
+        root = tree.getroot()
+
+        # Auto-set the showanswer value
+        if allowDownloads.lower() == 'true':
+            root.set('download_track', 'true')
+            root.set('download_video', 'true')
+        elif allowDownloads.lower() == 'false':
+            root.set('download_track', 'false')
+            root.set('download_video', 'false')
+        elif allowDownloads.lower() == 'video':
+            root.set('download_track', 'false')
+            root.set('download_video', 'true')
+        elif allowDownloads.lower() == 'transcript':
+            root.set('download_track', 'true')
+            root.set('download_video', 'false')
+        elif allowDownloads.lower() == 'reset':
+            try:
+                del root.attrib['download_track']
+                del root.attrib['download_video']
+            except:
+                pass
+        else:
+            sys.exit(instructions)
+
+        # Save the file
+        tree.write(os.path.join(dirpath, eachfile), encoding='UTF-8', xml_declaration=False)
