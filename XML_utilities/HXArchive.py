8cbf0cc1b1b0ac40e97021fe73b09420007c6a66
Colin-Fredericks <colin.fredericks@gmail.com>
2017-09-01 15:03:42 -0400

    All in one step

---
+++ XML_utilities/HXArchive.py
@@ -0,0 +1,13 @@
+import sys
+import Make_Course_Sheet
+import json2srt
+import SrtRename
+
+# Make the video spreadsheet
+Make_Course_Sheet.Make_Course_Sheet(sys.argv)
+# Transform all the .sjson files to .srt
+json2srt.json2srt(sys.argv + ['-r'])
+# Rename (copy) the SRT files to match our upload names
+SrtRename.SrtRename(sys.argv + ['-c'])
+#Done!
+print 'SRT archive prep complete.'
