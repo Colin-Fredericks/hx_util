458639819d54011bd97af5c0c390b90bc52d4384
Colin-Fredericks <colin.fredericks@gmail.com>
2017-09-28 13:15:01 -0400

    Now uses argparse for arguments.

--- XML_utilities/SetMaxAttempts.py
+++ XML_utilities/SetMaxAttempts.py
@@ -2,6 +2,7 @@
 import xml.etree.ElementTree as ET
 import sys
 import os
+import argparse
 
 instructions = """
 To use:
@@ -9,8 +10,9 @@ python SetMaxAttempts.py number path/to/problem/folder
 
 Your options for the number are:
   - An actual number, which sets all problems to the same # of attempts
-  - delete or default, which removes the value to allow the course-wide default to take over
-  - auto (recommended), in which case attempts are set as follows:
+  - "delete" or "default", which removes the value so that
+    course-wide default takes over
+  - "auto" (recommended), in which case attempts are set as follows:
     - Numerical and Formula problems get 10
     - Customresponse and Text problems get 5
     - Checkbox problems get a number of attempts equal to the number of choices, max 5.
@@ -19,8 +21,10 @@ Your options for the number are:
       - 2 if they have 4-6 options
       - 1 if they have 2-3 options
     - Multi-problems get the highest number of the set.
+    - Other problem types are skipped.
 
-There will probably still be some issues, so you'll need to review. This code will help you make a first pass, not a final pass.
+There will probably still be some issues, so you'll need to review.
+This code will help you make a first pass, not a final pass.
 """
 
 # Here are all the problem types we work on:
@@ -33,17 +37,25 @@ allProbtypes = ['multiplechoiceresponse',
 ]
 
 
+parser = argparse.ArgumentParser(usage=instructions, add_help=False)
+parser.add_argument('-h', '--help', action='store_true')
+parser.add_argument('number', default='auto')
+parser.add_argument('directory', default='.')
 
-# Get directory from command line argument
-try:
-    numberAttempts = sys.argv[1]
-    directory = sys.argv[2]
-except IndexError:
-    # Probably the wrong number of arguments
+args = parser.parse_args()
+if args.help:
     sys.exit(instructions)
+numberAttempts = args.number.lower()
+
+
+if not os.path.exists(args.directory):
+    sys.exit('Directory not found: ' + args.directory)
+
+numfiles = 0
+
 
 # Walk through the problems folder
-for dirpath, dirnames, filenames in os.walk(directory):
+for dirpath, dirnames, filenames in os.walk(args.directory):
     for eachfile in filenames:
         thisProbType = []
 
@@ -52,11 +64,11 @@ for dirpath, dirnames, filenames in os.walk(directory):
         root = tree.getroot()
 
         # If this isn't a problem file, skip it.
-        if root.tag is not 'problem':
+        if root.tag != 'problem':
             continue
 
         # Auto-set the showanswer value
-        if numberAttempts.lower() == 'auto':
+        if numberAttempts == 'auto':
 
             # Check the problem type
             for probtype in allProbtypes:
@@ -78,7 +90,6 @@ for dirpath, dirnames, filenames in os.walk(directory):
             if 'choiceresponse' in thisProbType:
                 root.set('max_attempts', str(numberOptions) if numberOptions <= 5 else '5')
 
-
             if 'customresponse' in thisProbType or 'stringresponse' in thisProbType:
                 root.set('max_attempts', '5')
 
@@ -86,7 +97,7 @@ for dirpath, dirnames, filenames in os.walk(directory):
                 root.set('max_attempts', '10')
 
         # Remove the max_attempts value to allow unlimited attempts or course default
-        elif numberAttempts.lower() == 'default' or numberAttempts.lower() == 'default':
+        elif numberAttempts == 'default' or numberAttempts == 'default':
             try:
                 del root.attrib['max_attempts']
             except:
@@ -99,3 +110,10 @@ for dirpath, dirnames, filenames in os.walk(directory):
 
         # Save the file
         tree.write(os.path.join(dirpath, eachfile), encoding='UTF-8', xml_declaration=False)
+        numfiles += 1
+
+
+if numfiles == 0:
+    print 'No files found - wrong or empty directory?'
+else:
+    print 'Max Attempts set for ' + str(numfiles) + ' files.'
